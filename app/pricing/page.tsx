220f0a3ec49565b0689ffb0de14b5621aa9ae64e
Declan Fisher <decfishe@amazon.com>
2024-08-22 07:17:47 +0100

    feat: Update pricing menu buttons

--- app/pricing/page.tsx
+++ app/pricing/page.tsx
@@ -1,5 +1,7 @@
-import Link from "next/link";
-import styles from "./page.module.css";
+import Link from 'next/link';
+import styles from './page.module.css';
+import AttachMoneyIcon from '@mui/icons-material/AttachMoney';
+import CardGiftcardIcon from '@mui/icons-material/CardGiftcard';
 
 export default function Pricing() {
     return (
@@ -9,14 +11,13 @@ export default function Pricing() {
                 <p>Manage pricing, discount and delivery for products</p>
             </div>
             <div className={styles.buttons}>
-                <Link href="/pricing/prices">
-                    <button>Update prices</button>
+                <Link href='/pricing/prices' className={styles.link}>
+                    <AttachMoneyIcon fontSize='large'/>
+                    <p>Update Prices</p>
                 </Link>
-                <Link href="/pricing/discount">
-                    <button>Update discounts</button>
-                </Link>
-                <Link href="/pricing/delivery">
-                    <button>Update delivery charge</button>
+                <Link href='/pricing/discount' className={styles.link}>
+                    <CardGiftcardIcon fontSize='large'/>
+                    <p>Update Discounts</p>
                 </Link>
             </div>
         </div>
